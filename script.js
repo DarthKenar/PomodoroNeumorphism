564d3f9cd12abd2a315cdfd78e3801027cf17335
Federico <federico_vega22@hotmail.com>
2023-12-01 15:14:00 -0300

    responsive

---
+++ script.js
@@ -0,0 +1,10 @@
+function start(){
+    //comenzar el tiempo
+    //si es mayor a 0
+        //reduce 1 el timer
+    //sino 
+        //parar tiempo 
+        //llamar alarma
+        //notificacion
+        //etc
+}
