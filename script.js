1f133b0fba7fb375b281e4e3b3cafe8e3af4940f
Federico <federico_vega22@hotmail.com>
2023-12-02 17:17:34 -0300

    script

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 function start(){
+
+    var runningTime = setInterval(()=>clockValue.textContent-=1,1000)
+
+    //ocultar botón
+    //mostrar boton parar
+    
     //comenzar el tiempo
-    //si es mayor a 0
-        //reduce 1 el timer
-    //sino 
-        //parar tiempo 
-        //llamar alarma
-        //notificacion
-        //etc
+    
+    let clockValue = document.getElementById("clockValue")
+    console.log(clockValue.textContent)
+    if (clockValue.textContent > 0){
+        var runningTime = setInterval(()=>clockValue.textContent-=1,1000)
+        if(clockValue.textContent < 1){
+            //llamar alarma
+        }
+        //si es mayor a 0
+            
+        //sino 
+            //parar tiempo 
+            //llamar alarma
+            //notificacion
+            //etc
+    }else{
+        clearInterval(runningTime)
+        //mostrar un cartel que diga establecer temporizador
+        // o que se establezca automaticamente
+    }
+
 }
+function checkClock(){
+    let clockValue = document.getElementById("clockValue")
+    if(clockValue < 1){
+        clearInterval(runningTime)
+    };
+};
+document.getElementById("clockValue").addEventListener('change', checkClock())
