c69a6ed789661dcdd549d72c4c1c929398c5060b
Federico Vega <federico_vega22@hotmail.com>
2023-12-02 22:04:14 -0300

    clock

--- script.js
+++ script.js
@@ -1,37 +1,17 @@
 function start(){
 
     var runningTime = setInterval(()=>clockValue.textContent-=1,1000)
-
-    //ocultar botón
-    //mostrar boton parar
-    
-    //comenzar el tiempo
-    
-    let clockValue = document.getElementById("clockValue")
-    console.log(clockValue.textContent)
-    if (clockValue.textContent > 0){
-        var runningTime = setInterval(()=>clockValue.textContent-=1,1000)
-        if(clockValue.textContent < 1){
-            //llamar alarma
-        }
-        //si es mayor a 0
-            
-        //sino 
-            //parar tiempo 
-            //llamar alarma
-            //notificacion
-            //etc
-    }else{
-        clearInterval(runningTime)
-        //mostrar un cartel que diga establecer temporizador
-        // o que se establezca automaticamente
-    }
+    //sitch por boton parar
+    //parar tiempo 
+    //llamar alarma
+    //notificacion
+    //etc
 
 }
-function checkClock(){
-    let clockValue = document.getElementById("clockValue")
+function checkClock(clockValue){
     if(clockValue < 1){
         clearInterval(runningTime)
     };
 };
-document.getElementById("clockValue").addEventListener('change', checkClock())
+var clockValue = document.getElementById("clockValue")
+clockValue.addEventListener('change', checkClock(clockValue))
