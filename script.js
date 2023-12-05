fbc693a81d42f558224b81c0a45939cf00508729
Federico Vega <federico_vega22@hotmail.com>
2023-12-04 22:44:44 -0300

    Modal Added

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var runningTime;
 var clockValue = document.getElementById("clockValue");
 var startButton = document.getElementById("startButton");
 var stopButton = document.getElementById("stopButton")
+var customizeButton = document.getElementById("customizeButton")
+
 function switchStartStopButton() {
     startButton.classList.toggle("hidden");
     stopButton.classList.toggle("hidden");
@@ -34,5 +36,22 @@ function stop() {
     switchStartStopButton();
     console.log("Stopped running time forced by user");
 }
+
+function customizeOpen(){
+    document.getElementById('customizeModal').style.display = 'block';
+}
+
+function customizeClose(){
+    document.getElementById('customizeModal').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    var modal = document.getElementById('customizeModal');
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
 startButton.addEventListener('click', start);
 stopButton.addEventListener('click', stop);
+customizeButton.addEventListener('click', customizeOpen);
