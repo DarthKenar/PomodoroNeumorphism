b87bcaf554b6a9ed14cc20a41afc3abc130176f8
Federico <federico_vega22@hotmail.com>
2023-12-05 12:02:21 -0300

    wip: Clock functionality

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-var initialValue = 60;
+
 var runningTime;
-var clockValue = document.getElementById("clockValue");
+var clockValueMin = document.getElementById("clockValueMin");
+var clockValueSeg = document.getElementById("clockValueSeg");
+var initialValueMin = clockValueMin.textContent
+var initialValueSeg = clockValueSeg.textContent
 var startButton = document.getElementById("startButton");
 var stopButton = document.getElementById("stopButton")
 var customizeButton = document.getElementById("customizeButton")
+var customizeModal = document.getElementById("customizeModal")
+var customizeOptionsList = customizeModal.getElementsByTagName("input")
+var eventoClick = new Event('click');
 
 function switchStartStopButton() {
     startButton.classList.toggle("hidden");
@@ -11,9 +17,8 @@ function switchStartStopButton() {
 }
 
 function start() {
-    if (clockValue.textContent > 0) {
+    if (clockValueMin.textContent > 0) {
         runningTime = setInterval(() => {
-            clockValue.textContent -= 1;
             checkClock();
         }, 1000);
         console.log("Started Running time");
@@ -22,17 +27,33 @@ function start() {
 }
 
 function checkClock() {
-    if (clockValue.textContent < 1) {
+    // checkClock se ejecuta a cada segundo
+    if (clockValueMin.textContent < 1 && clockValueSeg.textContent < 1) {
         clearInterval(runningTime);
-        clockValue.textContent = initialValue
+        clockValueMin.textContent = initialValueMin
+        clockValueSeg.textContent = initialValueSeg
         switchStartStopButton();
+        //Ejecutar Notificacion y alarma cuanto todo llega a 0
         console.log("Stopped running time automatically");
-    }
+        audio = new Audio("https://drive.google.com/file/d/1xQ6D76vi0_1byf3nM5CV9xfbDxQM5Rdh/view")
+        audio2 = new Audio("https://www.mediafire.com/file/j5qcrwepzlizc7f/alarm-clock-short-6402.mp3")
+        audio.play();
+        audio2.play();
+    }else if(clockValueMin.textContent > 1 && clockValueSeg.textContent < 1){
+        clockValueMin.textContent -= 1
+        clockValueSeg.textContent = 59
+    }else{
+        clockValueSeg.textContent -= 1;
+        if(clockValueSeg.textContent.length == 1){
+            clockValueSeg.textContent = "0"+clockValueSeg.textContent
+        };
+    };
 }
 
 function stop() {
     clearInterval(runningTime);
-    clockValue.textContent = initialValue
+    clockValueMin.textContent = initialValueMin
+    clockValueSeg.textContent = initialValueSeg
     switchStartStopButton();
     console.log("Stopped running time forced by user");
 }
@@ -52,6 +73,60 @@ window.onclick = function(event) {
     }
 }
 
+function selectOption(e){
+    if(customizeOptionsList[e].checked){
+        for(let i=0; i<customizeOptionsList.length; i++){
+            if(i!=e){
+                customizeOptionsList[i].checked = false
+            };
+        };
+        switch(e) {
+            case 0:
+                initialValueMin = 10;
+                initialValueSeg = 0;
+                clockValueMin.textContent = initialValueMin
+                clockValueSeg.textContent = initialValueSeg
+                break;
+            case 1:
+                initialValueMin = 20;
+                initialValueSeg = 0;
+                clockValueMin.textContent = initialValueMin
+                clockValueSeg.textContent = initialValueSeg
+                break;
+            case 2:
+                initialValueMin = 40;
+                initialValueSeg = 0;
+                clockValueMin.textContent = initialValueMin
+                clockValueSeg.textContent = initialValueSeg
+                break;
+            case 3:
+                initialValueMin = 60;
+                initialValueSeg = 60;
+                clockValueMin.textContent = initialValueMin
+                clockValueSeg.textContent = initialValueSeg
+                break;
+            default:
+                console.log("el switch no coincide con ningun valor")
+        }
+    }else{
+        setTimeout(function(){
+            customizeOptionsList[e].checked = true
+        },300)
+    }
+    if(!stopButton.classList.contains("hidden")){
+        stop()
+    }
+}
+
 startButton.addEventListener('click', start);
 stopButton.addEventListener('click', stop);
 customizeButton.addEventListener('click', customizeOpen);
+
+for(let i=0; i<customizeOptionsList.length; i++){
+    customizeOptionsList[i].addEventListener("change", function() {
+        selectOption(i);
+    });
+    if(i == 1){
+        customizeOptionsList[i].defaultChecked = true
+    }
+}
