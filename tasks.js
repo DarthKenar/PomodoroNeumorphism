f9252c8bf26cfe1a3aa90e6116c084d2a20290a6
Federico Vega <federico_vega22@hotmail.com>
2023-12-06 18:31:50 -0300

    WIP Delete UI TASK

--- tasks.js
+++ tasks.js
@@ -1,5 +1,6 @@
 var newTask = document.getElementById("newTask")
 var newTaskButton = document.getElementById("newTaskButton")
+var tasksList = document.getElementById("tasks")
 //Existir 2 listas:
     //Tareas completadas
     //Tareas Pendientes
@@ -21,20 +22,52 @@ function refreshTasks(){
     console.log("refreshTasks()")
 };
 
+function deleteTask(id){
+    let elementToRemove = document.getElementById(`pendingTask${id}`)
+    let elementToRemoveChildrens = elementToRemove.children;
+
+    elementToRemove.classList.add("disappear")
+    setTimeout(()=>(elementToRemove.classList.add("reduce")),1000)
+    
+    for (let i = 0; i < elementToRemoveChildrens.length; i++) {
+      caja.classList.add('reduce');
+      elementToRemoveChildrens[i].classList.add('reduce');
+      console.log(elementToRemoveChildrens[i])
+    }
+    
+}
+
 function saveTask(){
     console.log("saveTask()")
     
     //funcion que tendrá que:
         //Guardar la tarea en la lista tareas pendientes (pushear tarea)
-        pendingTasks.push(newTask.textContent);
-        // newTask.outerHTML("<li>HOLA A TODOS</li>")
-        // newTask.insertAdjacentHTML(outerHTML,"<li>HOLA A TODOS</li>");
-        // newTask.insertBefore();
-        //funcion:
-        refreshTasks();
-        // FocusEvent.click() //quitar el focus del elemento
-        newTask.focus()
-        newTask.textContent = ""
+    pendingTasks.push(newTask.textContent);
+    task = `<li id="pendingTask${pendingTasks.length}">
+                <div>
+                    <button class="switch" onclick="upTask(${pendingTasks.length})">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>
+                    </button>
+                    <button class="switch" onclick="downTask(${pendingTasks.length})">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+                    </button>
+                </div>
+                <label>${pendingTasks[pendingTasks.length-1]}</label>
+                <div>
+                    <button class="switch" onclick="editTask(${pendingTasks.length})">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg>
+                    </button>
+                    <button class="switch" onclick="deleteTask(${pendingTasks.length})">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+                    </button>
+                </div>
+            </li>`
+    tasksList.insertAdjacentHTML('beforeend',task)
+    //funcion:
+    refreshTasks();
+    // FocusEvent.click() //quitar el focus del elemento
+    newTask.focus()
+    newTask.textContent = ""
         
     //Limpiar newTask Label
         
