96a7c6fb680e5e80c1c97e357ff472b897490193
Federico <federico_vega22@hotmail.com>
2023-12-11 10:44:40 -0300

    WIP Lenguages

--- lenguagues.js
+++ lenguagues.js
@@ -1,5 +1,6 @@
 //modal
 var lenguageModal = document.getElementById('lenguageModal');
+var customizeModal = document.getElementById("customizeModal")
 
 //lenguage buttons (display modal)
 var lenguageButtonOpen = document.getElementById("lenguageButtonOpen")
@@ -9,25 +10,36 @@ var lenguageButtonClose = document.getElementById("lenguageButtonClose")
 var esBtn = document.getElementById('esBtn')
 var enBtn = document.getElementById('enBtn')
 
-//lenguage labels
+// Textos para traducir
+
+//Títulos de seleccion de lenguaje
 var esLabel = document.getElementById('esLabel')
 var enLabel = document.getElementById('enLabel')
 
-//titles
+//Títulos de pagina y cuerpo de html
 var title1 = document.getElementById('title1')
 var title2 = document.getElementById('title2')
 
-//options 
-var option1 = document.getElementById('option1')
-var option2 = document.getElementById('option2')
-var option3 = document.getElementById('option3')
-var option4 = document.getElementById('option4')
+//Opciones del temporizador (texto)
+var clockOption1 = document.getElementById('clockOption1')
+var clockOption2 = document.getElementById('clockOption2')
+var clockOption3 = document.getElementById('clockOption3')
+var clockOption4 = document.getElementById('clockOption4')
+
+// Título de la seccion tareas (tasks)
+var tasksLabel = document.getElementById("tasksLabel")
+
+//Navbar textos
+var customizeLabel = document.getElementById("customizeLabel")
+var restartSessionLabel = document.getElementById("restartSessionLabel")
+var lenguageLabel = document.getElementById("lenguageLabel")
+
+//newTask texto de invitacion (escribe aqui...)
+
 
 
 //for close any modals
 window.onclick = function(event) {
-    let lenguageModal = document.getElementById("lenguageModal")
-    let customizeModal = document.getElementById("customizeModal")
     if (event.target == lenguageModal) {
         lenguageModal.style.display = 'none';
     }
@@ -37,12 +49,23 @@ window.onclick = function(event) {
 }
 
 function lenguageOpen(){
+    console.log("Modal Opened")
     lenguageModal.style.display = 'block';
 }
 
 function lenguageClose(){
+    console.log("Modal Opened")
     lenguageModal.style.display = 'none';
 }
 
 lenguageButtonOpen.addEventListener('click',lenguageOpen)
 lenguageButtonClose.addEventListener('click',lenguageClose)
+
+// Spanish
+function spanishLenguageSelected(){
+    console.log("Switched to Spanish")
+};
+// English
+function englishLenguageSelected(){
+    console.log("Switched to English")
+};
